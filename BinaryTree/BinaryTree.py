e3ee57e781a134631ca193152fe0d061f7452c89
Aakash Wadhwa <aakash823@gmail.com>
2020-03-22 18:24:39 +0530

    Tree questions

--- BinaryTree/BinaryTree.py
+++ BinaryTree/BinaryTree.py
@@ -28,6 +28,21 @@ class BinaryTree:
             if current.right is not None:
                 queue.append(current.right)
         return array
+
+
+    def lowestcommonnode(self,root,val1,val2):
+        if root is None:
+            return
+        if root.data == val1 or root.data == val2:
+            return root.data
+        lefttree = self.lowestcommonnode(root.left,val1,val2)
+        righttree = self.lowestcommonnode(root.right,val1,val2)
+
+        if lefttree and righttree:
+            return root.data
+        if lefttree is None and righttree is None:
+            return None
+        return lefttree if lefttree is not None else righttree
         #1
     #   / \
     #  2   3
@@ -40,7 +55,14 @@ BT.root.left.left = Node(4)
 BT.root.left.right = Node(5)
 BT.root.right.left = Node(6)
 BT.root.right.right = Node(7)
+BT.root.left.left.left = Node(9)
+BT.root.left.left.right = Node(11)
+BT.root.left.right.right = Node(20)
+
+
+
 inordertravesal = BT.printdata(BT.root,[])
 print(inordertravesal)
 levelordertraversal = BT.levelorder(BT.root,[])
 print(levelordertraversal)
+print(BT.lowestcommonnode(BT.root,11,20))
