a37cb3254c315b3513b6b1ae7f7111bfc60eaf49
Aakash Wadhwa <aakash823@gmail.com>
2020-03-28 20:36:43 +0530

    adding questions

---
+++ practiceques/validateparanthesis.py
@@ -0,0 +1,22 @@
+def balancedBrackets(string):
+    
+	closing = '}])'
+	opening = '[{('
+	matching = {
+		')': '(',
+		'}': '{',
+		']':'['
+	}
+	stack = []
+	
+	for i in range(len(string)):
+		if string[i] in opening:
+			stack.append(string[i])
+		else:
+			if len(stack) == 0:
+				return False
+			elif stack[-1] == matching[string[i]]:
+				stack.pop()
+			else:
+				return False
+	return len(stack)==0
