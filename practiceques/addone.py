bf3db5fe0f230989926134a10cdb45384be2db6f
Aakash Wadhwa <aakash823@gmail.com>
2020-04-10 00:07:38 +0530

    adding one to array

--- practiceques/addone.py
+++ practiceques/addone.py
@@ -1,24 +1,23 @@
 def addone(array):
-    carry = 0
-    k = len(array) - 1 
-    array[k] += 1
-    if array[k] == 10:
+    if array[len(array)-1] == 9:
         carry = 1
-        array[k] = 0
-    for i in range(k-1,0,-1):
-        if array[i] + carry == 10:
-            carry = 1
-            array[i] = 0
-        else:
-            array[i] += carry
-            carry = 0
-    if array[0] + carry == 10:
-        array[0] = 0
-        array.append(1)
-        for i in range(k+1,0,-1):
-            array[i] = array[i-1]
-        array[0] = 1
-
-
+        for i in range(len(array)-1,0,-1):
+            if array[i] + carry == 10:
+                carry = 1
+                array[i] = 0
+            else:
+                array[i] += carry
+                carry = 0
+        if array[0] + carry == 10:
+            array.append(1)
+            array[0] = 0
+            for i in range(len(array)-1,0,-1):
+                array[i] = array[i-1]
+            array[0] = carry
+    else:
+        array[len(array)-1] +=1
     return array
-print(addone([1,3,4]))    
+
+
+
+print(addone([9,9,9,9,9]))    
